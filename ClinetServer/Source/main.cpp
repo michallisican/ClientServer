1ec6a678840a83e91ad7cdbec613d37b5117212f
michal.lisican <michallisican@gmail.com>
2017-10-18 20:22:20 +0200

    --Added new files

--- ClinetServer/Source/main.cpp
+++ ClinetServer/Source/main.cpp
@@ -20,23 +20,6 @@
 
 int main(int argc, char *argv[])
 {
-
-  /*std::string default_console = "\033["+Log::ItoS(0)+"m";
-      for (int i = 30; i <= 37; i++)
-      {
-          std::string color = "\033[0;"+Log::ItoS(i)+"m";
-
-          std::cout<<color<<"test "<<i<<std::endl;
-
-          color = "\033[1"+Log::ItoS(i)+"m";
-
-          std::cout<<color<<"test "<<i<<" bold"<<std::endl;
-          std::cout<<default_console<<std::endl;
-
-      }
-
-      std::cout << "Done" << std::endl;*/
-
   Log( "Program started", normal);
   std::string IP;
   int PORT;
